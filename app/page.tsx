3710888ee1a33cab4abe6d5526131e29fd15bf90
Jecho Torrefranca <jechotorrefranca.dvcode@gmail.com>
2026-03-22 04:14:57 +0800

    💄 Page Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,43 @@
 "use client";
 import Image from "next/image";
 import SignOutButton from "@/components/SignOutButton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
-    <>
-      <header className="sticky top-0 z-10 bg-background/80 backdrop-blur-md p-4 border-b border-slate-200 dark:border-slate-700 flex flex-row justify-between items-center shadow-sm">
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-3">
-            <Image src="/convex.svg" alt="Convex Logo" width={32} height={32} />
-            <div className="w-px h-8 bg-slate-300 dark:bg-slate-600"></div>
-            <Image
-              src="/nextjs-icon-light-background.svg"
-              alt="Next.js Logo"
-              width={32}
-              height={32}
-              className="dark:hidden"
-            />
-            <Image
-              src="/nextjs-icon-dark-background.svg"
-              alt="Next.js Logo"
-              width={32}
-              height={32}
-              className="hidden dark:block"
-            />
-          </div>
-          <h1 className="font-semibold text-slate-800 dark:text-slate-200">
-            Convex + Next.js + Convex Auth
-          </h1>
-          
+    <div className="bg-white h-screen text-black flex flex-col items-center">
+      <div className="flex justify-between p-2 min-w-screen bg-gray-200 fixed">
+        <div className="flex justify-center items-center gap-2">
+          <Avatar size="lg">
+            <AvatarImage src="https://wwwimage-us.pplusstatic.com/thumbnails/photos/w1800-q80/promotion/01_spc_collection_us_july2025_coh_poster_stan.jpg?format=webp" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <h1>Welcome!</h1>
         </div>
         <SignOutButton />
-      </header>
-      <main className="p-8 flex flex-col gap-8">
-      </main>
-    </>
+      </div>
+
+      <div className="w-204 h-264 bg-gray-100 mt-25">
+        <div className="w-full h-full flex flex-col outline relative">
+          <img
+            src="pfpDefault.png"
+            className="w-55 h-55 object-cover rounded-full bg-white absolute left-15 top-12"
+          />
+
+          <div className="bg-[#26202B] h-[17%] w-full flex justify-end">
+            <div className="text-white text-5xl w-[60%] self-center">
+              <h1>JECHO PARAIRO TORREFRANCA</h1>
+            </div>
+          </div>
+
+          <div className="bg-white w-full h-[80%] flex">
+            <div className="w-[40%] bg-red-300 pl-15 pt-25">left</div>
+            <div className="w-[60%] bg-blue-300 pt-5">right</div>
+          </div>
+
+          <div className="bg-[#26202B] h-[3%]"></div>
+        </div>
+      </div>
+    </div>
   );
 }
-
--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -1,25 +1,26 @@
 import { useAuthActions } from "@convex-dev/auth/react";
 import { useConvexAuth } from "convex/react";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default function SignOutButton() {
   const { isAuthenticated } = useConvexAuth();
   const { signOut } = useAuthActions();
   const router = useRouter();
+
+  const handleSignout = () => {
+    void signOut().then(() => {
+      router.push("/signin");
+    });
+  };
+
   return (
-    <>
+    <div className="text-white">
       {isAuthenticated && (
-        <button
-          className="bg-slate-600 hover:bg-slate-700 dark:bg-slate-700 dark:hover:bg-slate-600 text-white rounded-lg px-4 py-2 text-sm font-medium transition-all duration-200 shadow-sm hover:shadow-md cursor-pointer"
-          onClick={() =>
-            void signOut().then(() => {
-              router.push("/signin");
-            })
-          }
-        >
+        <Button variant="outline" size="lg" onClick={handleSignout}>
           Sign out
-        </button>
+        </Button>
       )}
-    </>
+    </div>
   );
 }
